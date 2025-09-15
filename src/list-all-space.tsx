0c5b5a3aa61e8af8508847695a74c88d13ed278f
finger <fingergohappy@163.com>
2025-09-16 03:23:36 +0800

    Add 'destroy' action to space list for removing spaces using Yabai command

--- src/list-all-space.tsx
+++ src/list-all-space.tsx
@@ -1,13 +1,25 @@
 import SpaceList from "./components/space-list";
 import { focusSpace } from "./focus-space";
+import { runYabaiCommand } from "./helpers/scripts";
 import { ISpace, IWindow } from "./types/yabai";
 
+const destorySpace = async (space: ISpace) => {
+  const { stderr } = await runYabaiCommand(`-m space --destroy ${space.index}`);
+  if (stderr) {
+    throw new Error(stderr);
+  }
+};
 
 export default function Command() {
   return <SpaceList actions={[{
     title: "focus",
     onAction: (space: ISpace) => {
       focusSpace(space.index);
-    }
+    },
+  }, {
+    title: "destroy",
+    onAction: (space: ISpace) => {
+      destorySpace(space);
+    },
   }]} spaceFilter={(spaces: ISpace[]) => spaces} windowFilter={(windows: IWindow[]) => windows} />;
 }
